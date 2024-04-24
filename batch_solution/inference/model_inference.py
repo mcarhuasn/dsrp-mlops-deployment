1dfb5980aebe971402dbc058300b7d6ab208c36a
mcarhuasn <mcarhuasn@gmail.com>
2024-04-24 09:33:21 -0500

    Upload predictions to S3

--- batch_solution/inference/model_inference.py
+++ batch_solution/inference/model_inference.py
@@ -5,6 +5,7 @@ import os
 from loguru import logger
 import boto3
 import pickle
+import io
 
 config = dotenv.dotenv_values(".env")
 print(config.get("MLFLOW_TRACKING_URI"))
@@ -35,6 +36,22 @@ def read_csv_from_s3(bucket_name, file_key, aws_access_key_id=None, aws_secret_a
 
     return df
 
+def upload_csv_to_s3(dataframe: pd.DataFrame, bucket_name, path, aws_access_key_id=None, aws_secret_access_key=None):
+    # Create a connection to S3
+    if aws_access_key_id and aws_secret_access_key:
+        s3 = boto3.client('s3', aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key)
+    else:
+        s3 = boto3.client('s3')
+
+    csv_buffer = io.StringIO()
+    #dataframe.to_csv(csv_buffer)
+    dataframe.to_csv("tmp.csv")
+
+    #s3.Object(bucket_name, path).put(Body=csv_buffer.getvalue())
+    #s3.upload_file(csv_buffer.getvalue(), bucket_name, path)
+    s3.upload_file("tmp.csv", bucket_name, path)
+    
+
 logger.info("Reading data from S3")
 AWS_ACCESS_KEY = config.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config.get("AWS_ACCESS_SECRET")
@@ -42,11 +59,11 @@ ID_VAR = "id"
 TARGET_VAR = "loss"
 
 raw_data = read_csv_from_s3(
-                        bucket_name = config.get("S3_BUCKET_RAW_NAME"), 
-                        file_key = "raw/sample_train_v2.csv",
-                        aws_access_key_id = AWS_ACCESS_KEY,
-                        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
-                        )
+                            bucket_name = config.get("S3_BUCKET_RAW_NAME"), 
+                            file_key = "raw/sample_train_v2.csv",
+                            aws_access_key_id = AWS_ACCESS_KEY,
+                            aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+                            )
 
 logger.info("Cleaning data input")
 data = raw_data.replace("NA", 0)
@@ -70,4 +87,13 @@ vector_predictions = model.predict(data)
 predictions = raw_data[[ID_VAR, TARGET_VAR]]
 predictions['loss_prediction'] = vector_predictions
 print(predictions.head())
+
+logger.info("Uploading Predictions to S3 Bucket")
+upload_csv_to_s3(
+                dataframe=predictions,
+                bucket_name = config.get("S3_BUCKET_RAW_NAME"),
+                path = "predictions/predicciones_dsrp_mcn.csv",
+                aws_access_key_id = AWS_ACCESS_KEY,
+                aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+                )
 
